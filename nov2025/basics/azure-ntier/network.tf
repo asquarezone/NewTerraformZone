d6d7c843bc611b416e4254226188249735889dfa
shaikkhajaibrahim <shaikkhajaibrahim@gmail.com>
2025-11-12 09:38:52 +0530

    Added child resources as block and as different resources

--- nov2025/basics/azure-ntier/network.tf
+++ nov2025/basics/azure-ntier/network.tf
@@ -16,3 +16,77 @@ resource "azurerm_subnet" "subnets" {
   virtual_network_name = azurerm_virtual_network.base.name
 
 }
+
+## security group with no child resources
+resource "azurerm_network_security_group" "web" {
+  name                = "web"
+  resource_group_name = azurerm_resource_group.base.name
+  location            = azurerm_resource_group.base.location
+  security_rule {
+    name                       = "openssh"
+    description                = "ssh"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+    access                     = "Allow"
+    direction                  = "Inbound"
+    priority                   = 300
+  }
+
+  security_rule {
+    name                       = "openhttp"
+    description                = "ssh"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+    access                     = "Allow"
+    direction                  = "Inbound"
+    priority                   = 310
+  }
+
+}
+
+## security group with child resources
+resource "azurerm_network_security_group" "app" {
+  name                = "app"
+  resource_group_name = azurerm_resource_group.base.name
+  location            = azurerm_resource_group.base.location
+
+}
+
+
+resource "azurerm_network_security_rule" "app_ssh" {
+  name                        = "ssh"
+  resource_group_name         = azurerm_resource_group.base.name
+  network_security_group_name = azurerm_network_security_group.app.name
+  description                 = "open ssh"
+  protocol                    = "Tcp"
+  priority                    = 300
+  source_address_prefix       = "*"
+  source_port_range           = "*"
+  destination_address_prefix  = "*"
+  destination_port_range      = "22"
+  access                      = "Allow"
+  direction                   = "Inbound"
+
+}
+
+resource "azurerm_network_security_rule" "app_http" {
+  name                        = "http"
+  resource_group_name         = azurerm_resource_group.base.name
+  network_security_group_name = azurerm_network_security_group.app.name
+  description                 = "open http"
+  protocol                    = "Tcp"
+  priority                    = 310
+  source_address_prefix       = "*"
+  source_port_range           = "*"
+  destination_address_prefix  = "*"
+  destination_port_range      = "80"
+  access                      = "Allow"
+  direction                   = "Inbound"
+
+}
