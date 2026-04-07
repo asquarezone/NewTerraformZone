a49f3bfd7cffa889bcf2c61af59556157c7ef8e6
shaikkhajaibrahim <shaikkhajaibrahim@gmail.com>
2026-04-07 10:24:28 +0530

    Added dependencies

--- apr26/basics/vpc_aws/example.tfvars
+++ apr26/basics/vpc_aws/example.tfvars
@@ -1,3 +1,3 @@
-network_cidr = "10.10.0.0/16"
+network_cidr     = "10.10.0.0/16"
 network_name_tag = "my-first-vpc"
-region = "ap-south-1"
+region           = "ap-south-1"
--- apr26/basics/vpc_aws/main.tf
+++ apr26/basics/vpc_aws/main.tf
@@ -4,3 +4,32 @@ resource "aws_vpc" "ntier" {
     Name = var.network_name_tag
   }
 }
+
+resource "aws_subnet" "one" {
+  # dynamic value and it becomes a dependency
+  # this is referred as implicit dependency
+  vpc_id            = aws_vpc.ntier.id 
+  availability_zone = "ap-south-1a"
+  cidr_block        = "10.10.0.0/24"
+
+  tags = {
+    Name = "one"
+  }
+  # explicit dependency
+  depends_on = [ 
+    aws_vpc.ntier 
+  ]
+}
+
+resource "aws_subnet" "two" {
+  vpc_id            = aws_vpc.ntier.id # dynamic value
+  availability_zone = "ap-south-1b"
+  cidr_block        = "10.10.1.0/24"
+
+  tags = {
+    Name = "two"
+  }
+  depends_on = [ 
+    aws_vpc.ntier
+  ]
+}
