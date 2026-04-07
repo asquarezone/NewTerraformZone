8400937aeee6550217ced6271ff39cfa6cd0fbf0
shaikkhajaibrahim <shaikkhajaibrahim@gmail.com>
2026-04-07 09:52:15 +0530

    Added changes

--- apr26/basics/vpc_aws/main.tf
+++ apr26/basics/vpc_aws/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.39"
-    }
-  }
-}
-
-provider "aws" {
-  region = "ap-south-1"
-
-}
-
 resource "aws_vpc" "ntier" {
   cidr_block = "10.0.0.0/24"
   tags = {
