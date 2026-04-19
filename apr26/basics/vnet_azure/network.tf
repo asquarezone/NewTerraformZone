7ad9a2d57f00c3c0a8b59b440a1406be650279d0
shaikkhajaibrahim <shaikkhajaibrahim@gmail.com>
2026-04-19 09:57:43 +0530

    Added changes

---
+++ apr26/basics/vnet_azure/network.tf
@@ -0,0 +1,77 @@
+resource "azurerm_virtual_network" "ntier" {
+  name                = var.network_info.name
+  resource_group_name = azurerm_resource_group.base.name
+  location            = azurerm_resource_group.base.location
+  depends_on          = [azurerm_resource_group.base]
+  address_space       = [var.network_info.address_space]
+}
+
+
+resource "azurerm_subnet" "subnets" {
+  count                           = length(var.subnets)
+  name                            = var.subnets[count.index].name
+  resource_group_name             = azurerm_resource_group.base.name
+  virtual_network_name            = azurerm_virtual_network.ntier.name
+  address_prefixes                = [var.subnets[count.index].address_space]
+  default_outbound_access_enabled = var.subnets[count.index].is_public_subnet
+
+}
+
+# public ip
+resource "azurerm_public_ip" "web" {
+  name                = "web-ip"
+  resource_group_name = azurerm_resource_group.base.name
+  location            = azurerm_resource_group.base.location
+  allocation_method   = "Static"
+}
+
+
+# network interface
+resource "azurerm_network_interface" "web" {
+  name                = "web"
+  resource_group_name = azurerm_resource_group.base.name
+  location            = azurerm_resource_group.base.location
+  ip_configuration {
+    name                          = "web"
+    subnet_id                     = azurerm_subnet.subnets[0].id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.web.id
+  }
+
+}
+
+# network security group
+resource "azurerm_network_security_group" "web" {
+  name                = "web-nsg"
+  resource_group_name = azurerm_resource_group.base.name
+  location            = azurerm_resource_group.base.location
+  security_rule {
+    name                       = "openhttp"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "openssh"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+}
+
+resource "azurerm_network_interface_security_group_association" "web" {
+  network_interface_id      = azurerm_network_interface.web.id
+  network_security_group_id = azurerm_network_security_group.web.id
+
+}
